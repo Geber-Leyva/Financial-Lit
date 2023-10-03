3cd18972de8bd854f73bae8827531b3a1fde0972
clariiDom3 <cdominguez30@miners.utep.edu>
2023-10-03 13:39:26 -0600

    updated css

--- src/Topics.js
+++ src/Topics.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import budg from './pictures/budget.jpg'
-import sav from './pictures/save.jpg'
-import cred from './pictures/credit.jpg'
-import pf from './pictures/personalfinance.jpeg'
-import deb from './pictures/debt.png'
+import budget from './pictures/budget.jpg'
+import saving from './pictures/save.jpg'
+import credit from './pictures/credit.jpg'
+import personalFinance from './pictures/personalfinance.jpeg'
+import debt from './pictures/debt.png'
 import invest from './pictures/investing.jpg'
 import './Topics.css'
 import TopicCard from './TopicCard'
 
 
 const topics= [
-  {id: 1 , name: "Budget", image :budg,}, 
-  {id: 2 , name: "Save", image: sav,}, 
-  {id: 3 , name: "Personal Finance", image: pf,}, 
-  {id: 4 , name : "Credit", image: cred,}, 
-  {id: 5 , name : "Debt/Loan", image : deb,},
+  {id: 1 , name: "Budget", image :budget,}, 
+  {id: 2 , name: "Save", image: saving,}, 
+  {id: 3 , name: "Personal Finance", image: personalFinance,}, 
+  {id: 4 , name : "Credit", image: credit,}, 
+  {id: 5 , name : "Debt/Loan", image : debt,},
   {id: 6, name: "Investing", image: invest},
 ]
 
