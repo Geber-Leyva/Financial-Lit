43a9b062eae1ff1691c6c92ffcf26eb290a8c6f5
Geber-Leyva <gyleyva2@miners.utep.edu>
2023-10-08 12:15:00 -0600

    added image and boxes to info

--- src/Topic/TopicsPage.js
+++ src/Topic/TopicsPage.js
@@ -4,12 +4,15 @@ import { useLocation } from 'react-router-dom';
 
 function Next_Window() {
   const location = useLocation();
-  const { name, info } = location.state;
+  const { name, image, info} = location.state;
   const infoLines = info.split('\n');
 
   return (
     <div className='wholeApp'>
-      <h1 className='centered'>{name}</h1>
+        <div className='name-and-image'>
+            <h1 className='centered'>{name}</h1>
+            <img src={image} alt={name} className='profile-image' />
+        </div>
       <div className='info-container'>
         {infoLines.map((line, index) => (
           <p key={index}className='info-paragraph'>{line}</p>
