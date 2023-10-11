670e71bf0a28614a5aea5a84480752fd1c479f74
clariiDom3 <cdominguez30@miners.utep.edu>
2023-10-11 00:24:43 -0600

    added excel sheet

--- src/Topic/TopicsPage.js
+++ src/Topic/TopicsPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Topics.css';
 import { useLocation } from 'react-router-dom';
+import BudgetSheet from './BudgetSheet';
 
 function TopicsPage() {
   const location = useLocation();
-  const { name, image, info} = location.state;
+  const { id, name, image, info} = location.state;
   const infoLines = info.split('\n');
+
 
   return (
     <div className='wholeApp'>
@@ -17,6 +19,13 @@ function TopicsPage() {
         {infoLines.map((line, index) => (
           <p key={index}className='info-paragraph'>{line}</p>
         ))}
+        {id === 1 ? (
+          <div>
+            <BudgetSheet />
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div>
     </div>
   );
